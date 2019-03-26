924775b3842cd5703ded84fe0298675733b61230
watertien <tianxsh@mail2.sysu.edu.cn>
2019-03-26 15:10:30 +0800

    Add week4 hw and week5 in class nb

---
+++ cp_imports.py
@@ -0,0 +1,7 @@
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# Use retina backend for better figures
+%config InlineBackend.figure_format = 'retina'
+
